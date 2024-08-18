510570d8a407896235c721eb19e700084b69e2ce
TAHA LABRAHIMI <144866467+taha-labrahimi@users.noreply.github.com>
2024-08-18 11:46:35 +0100

    some modifs in VersementMapper.java

--- src/main/java/com/sorec/dashboardcashmanagement/mapper/VersementMapper.java
+++ src/main/java/com/sorec/dashboardcashmanagement/mapper/VersementMapper.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+
 @Component
 public class VersementMapper implements EntityMapper<Versement, VersementDTO>{
 
